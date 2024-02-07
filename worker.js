8571e8e1e59db96c92affbe471710f251ea0ec4c
Ardis Lu <ardisglu@gmail.com>
2024-02-06 20:41:36 -0800

    Delete unused worker

--- worker.js
+++
@@ -1,7 +0,0 @@
-addEventListener('message', async e => {
-  const sab = e.data;
-  const arr = new BigInt64Array(sab);
-  const wait = Atomics.wait(arr, 0, 100n);
-  console.log(wait);
-  close();
-});
